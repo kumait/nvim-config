d1e4819c7dca6c55966d21a9f4b924dbf991595a
kumait <kumaitm@gmail.com>
2025-03-04 07:27:53 -0500

    Add Telescope

---
+++ nvim/lua/kumait/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+  end,
+  opts = {
+    -- this will make nvim setup this plugin using options here
+  },
+}
